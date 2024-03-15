62fe48787b83c5b88f07d2797fd1933437aa2c46
JennieOliveira99 <jennyferoliveira7315@gmail.com>
2024-03-15 17:22:58 -0300

    novo layout formacao

--- script/script.js
+++ script/script.js
@@ -24,29 +24,6 @@ document.addEventListener("DOMContentLoaded", function(){
 });
 
 
-let textosDosAlertas = [
-  "Fundação de Programação:\nAlgoritmos e Lógica de Programação, Criação de Conteúdo Web, Redes e Internet, Estruturas de Dados com PHP, SQL Server e Management Studio, Engenharia de Software para a Web, Noções Básicas de Internet, Design Digital, Negócios e Marketing, Eletrônica, Criação de Negócios na Internet, Servidores e seus Sistemas Operacionais, Práticas de Gerenciamento de Projetos, Padrões de Design de Sites na Internet, Acessibilidade, Leitura e Produção de Texto, Inglês\n\nMatemática:\n Fundamentos da Matemática Elementar, Matemática Discreta, Estatística.\n\nSegurança:\n Segurança de Sistemas de Internet, Legislação Aplicada à Internet.\n\nDesenvolvimento: \nDesign de Navegação e Interação, Desenvolvimento para Dispositivos, Desenvolvimento para Servidores, Prototipagem e Testes de Usabilidade, Arquitetura Orientada a Serviços, Design de Localização, Padrões de Design de Sites na Internet (HTML, CSS, JavaScript).",
-  "Fundação de Programação:\n Desenvolvimento de Sistemas, Análise e Projeto de Sistemas, Internet e Protocolos, Fundamentos de Computação, Inglês Instrumental, Língua Portuguesa, Trabalho e Tecnologia, Técnicas de Programação, Programação e Algoritmos.\n\n Desenvolvimento: \nProgramação de Aplicativos Móveis I, II, III (Mobile) com Csharp, Programação Web (HTML, CSS e JavaScript), Banco de Dados I, II, III (MySQL), Design Digital (Gimp, Fireworks), Sistemas Embarcados, Qualidade de Software e Testes. ",
-  "CorelDraw pelo Instituto Share (concluído - 2020)\n\n Fundamentos de Cibersegurança pela LINUX FOUNDATION (concluído - 2024):\n Conscientização em Cibersegurança, Malware, Violações de Segurança Cibernética, Proteção e Prevenção, Segurança de Dispositivos Móveis, Segurança de Redes Sociais, Software de Prevenção.\n\n IA Generativa pelo LinkedIn Learning\n Fundamentos de Tecnologia de Motores de Busca, Fundamentos de IA Generativa."
-];
-
-// Obtendo uma coleção de elementos com a classe "link-grade"
-let botoesGrade = document.querySelectorAll(".link-grade");
-
-// Iterando sobre a coleção e adicionando um ouvinte de evento a cada botão
-botoesGrade.forEach(function(botao) {
-  botao.addEventListener('click', function() {
-    // Obtendo o índice do botão do atributo de dados
-    let indice = botao.getAttribute('data-indice');
-    
-    // Obtendo o texto do array com base no índice
-    let textoPersonalizado = textosDosAlertas[indice];
-    
-    // Exibindo alerta com o texto personalizado
-    alert(textoPersonalizado);
-  });
-});
-
 
 
 //pdf download
